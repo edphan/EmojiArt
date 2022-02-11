722f20180888d9e82f543a76aeec1266f44d307c
Edward Phan <76992905+edphan@users.noreply.github.com>
2022-02-10 17:53:55 -0800

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Edward Phan on 2022-02-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Edward Phan on 2022-02-10.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
